289f66e5ccf8a73029a6464a1dd8e13f656cfe82
aclist <92275929+aclist@users.noreply.github.com>
2023-06-23 01:40:36 +0900

    chore: bump version

--- megamod.user.js
+++ megamod.user.js
@@ -2,7 +2,7 @@
 // @name          kbin-megamod
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.1.0
+// @version       0.1.1
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
