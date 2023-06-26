6497e000b2395bfff702ae5783c970f4504678a8
aclist <92275929+aclist@users.noreply.github.com>
2023-06-26 19:05:21 +0900

    feat: helpbox

--- megamod.user.js
+++ megamod.user.js
@@ -2,7 +2,7 @@
 // @name          kbin-megamod
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.4.0
+// @version       0.5.0
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
@@ -26,7 +26,7 @@
 
     const version = GM_info.script.version;
     const tool = GM_info.script.name;
-    const manifest = "https://raw.githubusercontent.com/aclist/kbin-megamod/main/manifest.json";
+    const manifest = "https://raw.githubusercontent.com/aclist/kbin-megamod/3a81cc679e0d6a46e9d29fdbed8f251dc02114a3/manifest.json";
     const repositoryURL = "https://github.com/aclist/kbin-megamod/";
 
       /*object used for interpolation of function names*/
@@ -96,8 +96,12 @@ var json = await GM.getValue("json");
             width: 100%;
             padding: 2rem;
             display: grid;
-            grid-template-areas: "header header" "sidebar body";
-            grid-template-columns: 1fr 2fr;
+            grid-template-areas: "header header header" "sidebar body helpbox";
+            grid-template-columns: 0.5fr 3fr 3fr;
+        }
+        .megamods-list {
+        width:fit-content;
+        margin-left: 0.5rem;
         }
 
         .megamod-settings-modal-content {
@@ -108,21 +112,24 @@ var json = await GM.getValue("json");
         }
 
         .megamod-dock{
+            float:right;
             padding-left: 0.5rem;
+            padding-right: 0.5rem;
         }
         .megamod-dock:hover {
             opacity: 0.5;
         }
 
+       .megamod-header-hr {
+       border: 1px solid gray
+       }
+
         .megamod-version {
             float: right;
-            margin-right: 0.5rem;
-            padding-top: 0.1rem;
          }
         .megamod-settings-modal-content .close {
             color: #aaa;
             float: right;
-            font-size: 28px;
             cursor: pointer;
         }
         .megamod-settings-modal-content .close:hover{
@@ -157,23 +164,30 @@ var json = await GM.getValue("json");
         .megamod-settings-modal-body {
             grid-area: body;
         }
-
-        .megamod-settings-modal-body label .description {
-            transition: opacity 0.15s ease-in-out;
+         .megamod-settings-modal-helpbox {
+            grid-area: helpbox;
             opacity: 0;
+            padding: 1rem;
+            color: var(--kbin-header-text-color);
         }
 
         .megamod-settings-modal-body label:hover .description {
             opacity: 1;
         }
-
+        .megamod-settings-modal-body label .description {
+            transition: opacity 0.15s ease-in-out;
+            opacity: 0;
+        }
         .mod-author {
             float: right;
         }
+        .megamod-tooltip {
+        margin-left: 0.5rem;
+        }
         .megamod-tab-link {
              border-radius: 0px;
-             color: var(--kbin-header-text-color);
             border: none;
+            color: var(--kbin-header-text-color);
         }
         .megamod-tab-link:hover{
             opacity: 0.65;
@@ -212,42 +226,82 @@ var json = await GM.getValue("json");
 
         const header = document.createElement("div");
         header.className = "megamod-settings-modal-header";
-	/*TODO: check for new remote version at startup and insert link*/
         header.innerHTML = `
-            <span class="close">
-            <i class="fa-solid fa-times"></i>
-            </span>
+            <span class="close"><i class="fa-solid fa-times"></i></span>
+             <span class="megamod-dock"><i class="fa-solid fa-arrow-down"></i></span>
             <span class="megamod-version">` + tool + ' ' + version + `</span>
-            <span class="megamod-dock"><i class="fa-solid fa-arrow-down"></i></span>
-            <hr style="border: 1px solid gray">
-            <h2>Megamod Settings</h2>
-        `
+            `
+
+        const crumbs = document.createElement("div");
+        crumbs.className = 'megamod-crumbs';
+        let crumbsRoot = document.createElement('h2')
+        crumbsRoot.innerText = 'Megamod Settings'
+        crumbs.appendChild(crumbsRoot);
+        header.appendChild(crumbs)
+        const headerHr = document.createElement('hr');
+        headerHr.className = 'megamod-header-hr'
+        header.appendChild(headerHr);
+
         const sidebar = document.createElement("div");
         sidebar.className = "megamod-settings-modal-sidebar";
+        let sidebarUl = document.createElement('ul');
 
-        sidebar.innerHTML = `
-        <ul>
-        <li><a class="megamod-tab-link" onclick="openTab(event, 'general')">General</a></li>
-        <li><a class="megamod-tab-link" onclick="openTab(event, 'threads')">Threads</a></li>
-        <li><a class="megamod-tab-link" onclick="openTab(event, 'comments')">Comments</a></li>
-        <li><a class="megamod-tab-link" onclick="openTab(event, 'profiles')">Profiles</a></li>
-        <ul>`
+
+        const sidebarPages = [
+            "general",
+            "threads",
+            "comments",
+            "profiles"
+            ]
+        for ( let i = 0; i < sidebarPages.length; ++i) {
+            let page = sidebarPages[i];
+            let pageUpper = sidebarPages[i].charAt(0).toUpperCase() + sidebarPages[i].slice(1);
+            let sidebarListItem = document.createElement('li');
+            sidebarListItem.innerHTML = `
+            <a class="megamod-tab-link" onclick="openTab(event, '` + page + `')">` + pageUpper + `</a></li>`
+            sidebarUl.appendChild(sidebarListItem);
+        }
+       sidebar.appendChild(sidebarUl);
+
+         const hscript = document.createElement("script");
+        hscript.innerHTML = `
+        function openHelpBox(author, desc){
+        let kbinPrefix = 'https://kbin.social/u/';
+        let url = kbinPrefix + author;
+        let hBox = document.querySelector('.megamod-settings-modal-helpbox');
+        hBox.style.cssText = 'display: inline; opacity: 1;'
+        hBox.innerHTML = '<p>Author: ' + '<a href="' + url + '">' + author + '</a><br>Description: ' +  desc + '</p>';
+        };
+        `
+       const cscript = document.createElement("script");
+        cscript.innerHTML = `
+        function closeHelpBox(){
+        let hBox = document.querySelector('.megamod-settings-modal-helpbox');
+        hBox.style.cssText = 'display:none';
+        }
+        `
+
+        document.body.appendChild(hscript);
+        document.body.appendChild(cscript);
 
         // Add script tag with opentab function
         const script = document.createElement("script");
         script.innerHTML = `
-        
+
         function openTab(event, tabName) {
             event.stopPropagation();
             // Hide all options not in this tab (without this classname)
             const options = document.getElementsByClassName("megamods-list")[0];
             const optionsChildren = options.children;
-    
+
             for (let i = 0; i < optionsChildren.length; i++) {
                 if (optionsChildren[i].className.indexOf(tabName) > -1) {
                     optionsChildren[i].style.display = "block";
                 } else {
                     optionsChildren[i].style.display = "none";
+                    let crumbUpper = tabName.charAt(0).toUpperCase() + tabName.slice(1);
+                   let crumbsRoot = document.querySelector('.megamod-crumbs h2');
+                   crumbsRoot.innerText = 'Megamod Settings :: ' + crumbUpper;
                 }
             }
         }
@@ -260,23 +314,22 @@ var json = await GM.getValue("json");
         const megamodUl = document.createElement("ul")
               megamodUl.className = "megamods-list";
 
-        megamodUl.innerHTML = `
-        <li class="general"><h2>General</h2></li>
-        <li class="threads"><h2>Threads</h2></li>
-        <li class="comments"><h2>Comments</h2></li>
-        <li class="profiles"><h2>Profiles</h2></li>
-        `;
+        const helpBox = document.createElement("div");
+        helpBox.className = "megamod-settings-modal-helpbox";
+
 
         /*inject modal*/
         modal.appendChild(modalContent);
         modalContent.appendChild(header);
         modalContent.appendChild(sidebar);
+        modalContent.appendChild(helpBox);
         modalContent.appendChild(bodyHolder);
-        bodyHolder.appendChild(megamodUl);
+       bodyHolder.appendChild(megamodUl);
        document.body.appendChild(modal);
 
     /*populate modal identifiers*/
-    function insertListItem(func, item, desc,author,type,page){
+    /*TODO: extend boilerplate*/
+    function insertListItem(func, item, desc,author,type,page, it){
             switch(type) {
                 case 'checkbox': console.log('toggle type selected');
                 break;
@@ -285,12 +338,15 @@ var json = await GM.getValue("json");
             }
        const megamodListItem = document.createElement("li");
        megamodListItem.className = page;
-       megamodListItem.innerHTML+=`<label><input type="checkbox" id="megamod-option" megamod-entry="` + func + `"/input>` + item + `<span class="description">` + desc + ` <span class="mod-author">(<a href="https://kbin.social/u/` + author + '">' + author + `</a>)</span></span></label>`
-        megamodUl.appendChild(megamodListItem);
-    }
+       megamodListItem.innerHTML+=`<input type="checkbox" id="megamod-option" megamod-entry="` +
+           func + `"/input>` + item +
+           `<i class="megamod-tooltip fa-solid fa-question-circle" onclick="openHelpBox('` + author +`','` + desc + `')"></i>`
+       megamodUl.appendChild(megamodListItem);
+
+      }
 
     for (let i = 0; i < json.length; ++i) {
-                insertListItem(json[i].entrypoint, json[i].label, json[i].desc, json[i].author, json[i].type, json[i].page);
+                insertListItem(json[i].entrypoint, json[i].label, json[i].desc, json[i].author, json[i].type, json[i].page, i);
         let func = json[i].entrypoint;
      const check = document.querySelector(`[megamod-entry="` + func + `"]`);
             if (settings[func] == true) {
@@ -301,10 +357,9 @@ var json = await GM.getValue("json");
          }
 
         /*dock button*/
+        /*TODO: remember user's docked position*/
         modal.querySelector('.megamod-dock i').addEventListener("click", (e) =>{
-            console.log('click');
             let cn = e.target.className;
-            console.log(cn);
             if (cn == "fa-solid fa-arrow-down") {
                 modalContent.style.cssText = 'position:absolute;bottom:0;width:100%';
                 e.target.className = 'fa-solid fa-arrow-up';
@@ -314,6 +369,7 @@ var json = await GM.getValue("json");
                 e.target.className = 'fa-solid fa-arrow-down';
             }
         });
+
 
         /*close button*/
         modal.querySelector(".megamod-settings-modal .close").addEventListener("click", () => {
@@ -326,7 +382,7 @@ var json = await GM.getValue("json");
         });
 
          /*interpolate function names*/
-        document.querySelector('.megamods-list').onchange = function(e) {
+        document.querySelector('#megamod-option').onchange = function(e) {
         const settings = getSettings();
         var state
         var eventTarget = e.target;
