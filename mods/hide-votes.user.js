4faecbf169491713b553d0e84c525344c0752cb9
artillect <oreo4455@gmail.com>
2023-07-01 01:32:12 -0700

    fix: Corrected selectors for vote buttons

--- mods/hide-votes.user.js
+++ mods/hide-votes.user.js
@@ -12,20 +12,20 @@ const downvoteObserver = new MutationObserver(hideDownvotes);
 
 function hideUpvotes(toggle) {
     if (toggle) {
-        $('.vote__up').hide();
+        $('form.vote__up').hide();
         upvoteObserver.observe(document.body, { childList: true, subtree: true });
     } else {
-        $('.vote__up').show();
+        $('form.vote__up').show();
         upvoteObserver.disconnect();
     }
 }
 
 function hideDownvotes(toggle) {
     if (toggle) {
-        $('.vote__down').hide();
+        $('form.vote__down').hide();
         downvoteObserver.observe(document.body, { childList: true, subtree: true });
     } else {
-        $('.vote_down').show();
+        $('form.vote__down').show();
         downvoteObserver.disconnect();
     }
 }
