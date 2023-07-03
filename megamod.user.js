9c27cbe9ae4c414e8ffdf5be2bc7fc04ab023318
aclist <92275929+aclist@users.noreply.github.com>
2023-07-04 05:53:59 +0900

    fix: proper versioning in hook

--- megamod.user.js
+++ megamod.user.js
@@ -1,9 +1,8 @@
-
 // ==UserScript==
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.9.1
+// @version       0.10.1
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
