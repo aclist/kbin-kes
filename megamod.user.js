84ccdc997269202728d0996f483accf3e0f711bc
aclist <92275929+aclist@users.noreply.github.com>
2023-07-05 16:28:54 +0900

    fix: malformed JSON

--- megamod.user.js
+++ megamod.user.js
@@ -3,7 +3,7 @@
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.11.3
+// @version       0.11.4
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
