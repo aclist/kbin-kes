6fb8a784cb7043237f09c00c00e0a4c4a562adf8
aclist <92275929+aclist@users.noreply.github.com>
2023-07-05 16:37:27 +0900

    fix: add radio label

--- megamod.user.js
+++ megamod.user.js
@@ -2,7 +2,7 @@
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.11.6
+// @version       0.11.7
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
