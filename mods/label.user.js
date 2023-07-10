7e4a04d8983a7a93cdbc6f84def6b47bc52c7784
aclist <92275929+aclist@users.noreply.github.com>
2023-07-10 15:30:33 +0900

    fix: padding on OP label

--- mods/label.user.js
+++ mods/label.user.js
@@ -12,6 +12,6 @@ function labelOp(toggle){
     if (toggle === false) {
          document.styleSheets[0].addRule('.own.author > header > .user-inline::after','content: "";background-color:unset;padding-left: 0px !important');
     } else {
-        document.styleSheets[0].addRule('.own.author > header > .user-inline::after','content: " OP";color:white;background-color:#111;margin-left: 3px;padding-left: 3px;padding-right: 3px');
+        document.styleSheets[0].addRule('.own.author > header > .user-inline::after','content: " OP";color:white;background-color:#111;margin-left: 3px;padding-left: 1px;padding-right: 5px');
     }
 }
