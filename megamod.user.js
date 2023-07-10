550123f2c03da4cd478dfc5eaaaac6ec0055ed98
aclist <92275929+aclist@users.noreply.github.com>
2023-07-10 20:17:59 +0900

    fix: change initial value

--- megamod.user.js
+++ megamod.user.js
@@ -2,7 +2,7 @@
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.21.8
+// @version       0.21.9
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
