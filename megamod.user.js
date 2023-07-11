3c2dfa1298e4b12552101446e883a9b3b5c8f87e
aclist <92275929+aclist@users.noreply.github.com>
2023-07-11 16:41:54 +0900

    feat: add color parsing to label

--- megamod.user.js
+++ megamod.user.js
@@ -2,7 +2,7 @@
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.22.0
+// @version       0.23.0
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
