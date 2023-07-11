6539871f1dd44fa6a3e8ed63380d8fea904a09ae
aclist <92275929+aclist@users.noreply.github.com>
2023-07-11 16:37:11 +0900

    feat: add colorpicker to OP label

--- megamod.user.js
+++ megamod.user.js
@@ -2,7 +2,7 @@
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       0.21.12
+// @version       0.22.0
 // @description   megamod pack for kbin
 // @author        aclist
 // @match         https://kbin.social/*
