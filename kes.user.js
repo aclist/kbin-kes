5744735b085e2450eec371df4d45a2322e653339
aclist <92275929+aclist@users.noreply.github.com>
2023-07-12 22:47:08 +0900

    fix: escape backslashes

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       1.1.1
+// @version       1.1.2
 // @description   Kbin Enhancement Suite
 // @author        aclist
 // @match         https://kbin.social/*
