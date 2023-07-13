929a6524450992d57a69300b719be47a5c8e58c7
minnie <137783738+minnieoo@users.noreply.github.com>
2023-07-13 12:36:02 -0500

    Update resize-text.user.js

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -9,38 +9,11 @@
 // @match        https://karab.in/*
 // @match        https://www.kbin.cafe/*
 // @match        https://karab.in/*
-// @match        https://readit.buzz/*
-// @match        https://forum.fail/*
-// @match        https://fedi196.gay/*
-// @match        https://feddit.online/*
-// @match        https://kbin.run/*
-// @match        https://nadajnik.org/*
-// @match        https://kbin.cafe/*
-// @match        https://kbin.lol/*
-// @match        https://nerdbin.social/*
-// @match        https://kbin.lgbt/*
-// @match        https://kbin.place/*
-// @match        https://kopnij.in/*
-// @match        https://kbin.sh/*
-// @match        https://kayb.ee/*
-// @match        https://wiku.hu/*
-// @match        https://kbin.chat/*
-// @match        https://fediverse.boo/*
-// @match        https://tuna.cat/*
-// @match        https://kbin.dk/*
-// @match        https://kbin.projectsegau.lt/*
-// @match        https://bin.pol.social/*
-// @match        https://kbin.fedi.cr/*
-// @match        https://baguette.pub/*
-// @match        https://kbin.tech/*
 // @icon         https://kbin.social/favicon.svg
 // @grant        none
 // ==/UserScript==
 
-function resizeComments() {
-      const selectedSize = settings["size"];
-      const sizeValue = sizes[selectedSize];
-      
+function resizeComments() {   
       const sizes = {
         "Small": ".8rem",
         "Normal": ".9rem",
@@ -48,6 +21,9 @@ function resizeComments() {
         "Extra Large": "1.1rem",
         "Extra Extra Large": "1.3rem"
       };
+
+      const selectedSize = settings["size"];
+      const sizeValue = sizes[selectedSize];
     
       function iterateComments(param, size) {
         param.forEach(comment => {
