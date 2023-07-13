f00539873a30b01e3d63e3bf5acb2231d1681a5d
minnie <minnielovescoffee@gmail.com>
2023-07-13 13:46:48 -0500

    feat: add resize text mod script file

--- mods/resize-text.user.js
+++ mods/resize-text.user.js
@@ -14,51 +14,52 @@
 // ==/UserScript==
 
 function resizeText() {
-  const comment = document.querySelectorAll('blockquote.comment div');
-  const posts = document.querySelectorAll('div.content.formatted');
-  let settings = getModSettings("resize");
-  const sizes = {
-      "Small": ".8rem",
-      "Normal": ".9rem",
-      "Large": "1rem",
-      "Extra Large": "1.1rem",
-      "Extra Extra Large": "1.3rem"
-    };
-  const selectedSize = settings["size"];
-  const sizeValue = sizes[selectedSize];
-  
-  // call functions to modify text sizes depending on application choice
-    if (settings["option"] == "Only comments") {
-        resizeComments();
-    } else if (settings["option"] == "Only posts") {
-        resizePosts();
-    } else if (settings["option"] == "Comments and posts") {
-        resizeComments();
-        resizePosts();
-    }
-
-  // functions that modify text sizes based on choice
-  function resizeComments() {   
-      function iterateComments(param, size) {
-        param.forEach(comment => {
-          comment.style.fontSize = size;
-        });
-      }
+    const comment = document.querySelectorAll('blockquote.comment div');
+    const posts = document.querySelectorAll('div.content.formatted');
+    let settings = getModSettings("resize");
+    const sizes = {
+        "Small": ".8rem",
+        "Normal": ".9rem",
+        "Large": "1rem",
+        "Extra Large": "1.1rem",
+        "Extra Extra Large": "1.3rem"
+      };
+    const selectedSize = settings["size"];
+    const sizeValue = sizes[selectedSize];
     
-      iterateComments(comment, sizeValue);
-  }
-
-  function resizePosts() {
-    posts.forEach(post => {
-      post.style.fontSize = sizeValue;
-    })
-  }
-}
-
-
-function textResize(toggle) {
-    if (toggle) {
-        resizeText();
-    } else {
+    // call functions to modify text sizes depending on application choice
+      if (settings["option"] == "Only comments") {
+          resizeComments();
+      } else if (settings["option"] == "Only posts") {
+          resizePosts();
+      } else if (settings["option"] == "Comments and posts") {
+          resizeComments();
+          resizePosts();
+      }
+  
+    // functions that modify text sizes based on choice
+    function resizeComments() {   
+        function iterateComments(param, size) {
+          param.forEach(comment => {
+            comment.style.fontSize = size;
+          });
+        }
+      
+        iterateComments(comment, sizeValue);
     }
-}
+  
+    function resizePosts() {
+      posts.forEach(post => {
+        post.style.fontSize = sizeValue;
+      })
+    }
+  }
+  
+  
+  function textResize(toggle) {
+      if (toggle) {
+          resizeText();
+      } else {
+      }
+  }
+  
