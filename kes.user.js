e9545ded1c917e534518c3ef538d462f91b4b415
Brad Hughes <Oricul@users.noreply.github.com>
2023-07-13 10:50:55 -0500

    cleanup

--- kes.user.js
+++ kes.user.js
@@ -52,44 +52,6 @@
 // @downloadURL    https://github.com/aclist/kbin-scripts/raw/testing/kes.user.js
 // @updateURL      https://github.com/aclist/kbin-scripts/raw/testing/kes.user.js
 // ==/UserScript==
-let gmPrefix
-try {
-	if(GM_info){
-		gmPrefix = "GM_"
-	} else {
-		gmPrefix = "GM."
-	}
-} catch {
-	console.log(error);
-}
-    console.log("GM prefix is " + gmPrefix);
-    window.safeGM = function(func,...args){
-        let use
-        let underscore = {
-            setValue(...args) { return GM_setValue(...args) },
-            getValue(...args) { return GM_getValue(...args) },
-            addStyle(...args) { return GM_addStyle(...args)},
-            xmlhttpRequest(...args) { return GM_xmlhttpRequest(...args)},
-            setClipboard(...args) { return GM_setClipboard(...args)},
-            getResourceText(...args) { return GM_getResourceText(...args)},
-            info() { return GM_info }
-        }
-        let dot = {
-            setValue(...args) { return GM.setValue(...args) },
-            getValue(...args) { return GM.getValue(...args) },
-            addStyle(...args) { return GM.addStyle(...args)},
-            xmlhttpRequest(...args) { return GM.xmlHttpRequest(...args)},
-            setClipboard(...args) { return GM.setClipboard(...args)},
-            getResourceText(...args) { return GM.getResourceText(...args)},
-            info() { return GM_info }
-        }
-        if (gmPrefix === "GM_") {
-            use = underscore
-        } else {
-            use = dot
-        }
-        return use[func](...args);
-    }
 const version = safeGM("info").script.version;
 const tool = safeGM("info").script.name;
 const repositoryURL = "https://github.com/aclist/kbin-kes/";
