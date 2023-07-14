323bf11e67afc44a271707a550d78e327ad29f6b
aclist <92275929+aclist@users.noreply.github.com>
2023-07-14 17:42:15 +0900

    fix: better async handling

--- safegm.user.js
+++ safegm.user.js
@@ -14,7 +14,13 @@ if (gmPrefix === "GM_") {
 		gmPrefix = "GM."
 	}
 }
-    console.log("GM prefix is " + gmPrefix);
+
+function addCustomCSS(css){
+var style = document.createElement('style');
+style.innerHTML = css;
+document.head.appendChild(style);
+};
+
 window.safeGM = function(func,...args){
     let use
     let underscore = {
@@ -26,15 +32,16 @@ window.safeGM = function(func,...args){
         getResourceText(...args) { return GM_getResourceText(...args)},
         info() { return GM_info }
     }
-    let dot = {
+        let dot = {
         setValue(...args) { return GM.setValue(...args) },
         getValue(...args) { return GM.getValue(...args) },
-        addStyle(...args) { return GM.addStyle(...args)},
+        addStyle(...args) { return addCustomCSS(...args)},
         xmlhttpRequest(...args) { return GM.xmlHttpRequest(...args)},
         setClipboard(...args) { return GM.setClipboard(...args)},
         getResourceText(...args) { return GM.getResourceText(...args)},
         info() { return GM_info }
     }
+
     if (gmPrefix === "GM_") {
         use = underscore
     } else {
