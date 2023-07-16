40d533564a55e628062f596544c4df2923cde759
aclist <92275929+aclist@users.noreply.github.com>
2023-07-16 11:15:04 +0900

    fix: hotfix for #123

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name          KES
 // @namespace     https://github.com/aclist/
 // @license       MIT
-// @version       1.2.0
+// @version       1.2.1
 // @description   Kbin Enhancement Suite
 // @author        aclist
 // @match         https://kbin.social/*
--- mods/nav-icons.user.js
+++ mods/nav-icons.user.js
@@ -1,28 +1,15 @@
-function navbarIcons(toggle){
+function navbarIcons (toggle) {
     let settings = getModSettings("nav_icons");
     let search = settings.search
     let post = settings.post
     let subs = settings.subs
     if (toggle) {
-	if(document.querySelector('.subscription-panel-mobile-button')) {
-        document.styleSheets[0].addRule('header menu li:nth-of-type(2) .icon ::before','content: "' + search + '";');
-        document.styleSheets[0].addRule('header menu li:nth-of-type(3) .icon ::before','content: "' + post + '";');
-        document.styleSheets[0].addRule('header menu li:nth-of-type(4) .icon ::before','content: "' + subs + '";');
-	} else {
-        document.styleSheets[0].addRule('header menu li:nth-of-type(1) .icon ::before','content: "' + search + '";');
-        document.styleSheets[0].addRule('header menu li:nth-of-type(2) .icon ::before','content: "' + post + '";');
-        document.styleSheets[0].addRule('header menu li:nth-of-type(3) .icon ::before','content: "' + subs + '";');
-	}
+            document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content: "' + search + '";');
+            document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content: "' + post + '";');
+            document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', 'content: "' + subs + '";');
     } else {
-	if(document.querySelector('.subscription-panel-mobile-button')) {
-            document.styleSheets[0].addRule('header menu li:nth-of-type(2) .icon ::before','content:"\\f002" ;');
-	    document.styleSheets[0].addRule('header menu li:nth-of-type(3) .icon ::before','content:"\+" ;');
-	    document.styleSheets[0].addRule('header menu li:nth-of-type(4) .icon ::before','content:"\\f03a" ;');
-		
-	} else {
-            document.styleSheets[0].addRule('header menu li:nth-of-type(1) .icon ::before','content:"\\f002" ;');
-	    document.styleSheets[0].addRule('header menu li:nth-of-type(2) .icon ::before','content:"\+" ;');
-	    document.styleSheets[0].addRule('header menu li:nth-of-type(3) .icon ::before','content:"\\f03a" ;');
-	}
+            document.styleSheets[0].addRule('header menu li a[aria-label="Search"] i::before', 'content:"\\f002" ;');
+            document.styleSheets[0].addRule('header menu li a[aria-label="Add"] i::before', 'content:"\+" ;');
+            document.styleSheets[0].addRule('header menu li a[aria-label="Select a channel"] i::before', 'content:"\\f03a" ;');
     }
 }
