b72142f5573549cb536044ad65538895450c7b64
aclist <92275929+aclist@users.noreply.github.com>
2023-07-17 05:24:51 +0900

    chore: clean up safeGM

--- helpers/safegm.user.js
+++ helpers/safegm.user.js
@@ -5,15 +5,15 @@ try {
     if (GM_info) {
         let scriptHandler = GM_info.scriptHandler;
         switch (scriptHandler) {
-            case "Greasemonkey":
-                gmPrefix = dotPrefix;
-                break;
-            case "Userscripts":
-                gmPrefix = dotPrefix;
-                break;
-            default:
-                gmPrefix = underPrefix;
-                break;
+        case "Greasemonkey":
+            gmPrefix = dotPrefix;
+            break;
+        case "Userscripts":
+            gmPrefix = dotPrefix;
+            break;
+        default:
+            gmPrefix = underPrefix;
+            break;
         }
     }
 } catch (error) {
@@ -23,7 +23,7 @@ function addCustomCSS (css) {
     var style = document.createElement('style');
     style.innerHTML = css;
     document.head.appendChild(style);
-};
+}
 function genericXMLRequest (url, callback) {
     safeGM("xmlhttpRequest", {
         method: 'GET',
@@ -35,26 +35,26 @@ function genericXMLRequest (url, callback) {
         },
 
     });
-};
+}
 
-window.safeGM = function (func,...args){
+window.safeGM = function (func,...args) {
     let use
     let underscore = {
-        setValue(...args) { return GM_setValue(...args) },
-        getValue(...args) { return GM_getValue(...args) },
-        addStyle(...args) { return GM_addStyle(...args)},
-        xmlhttpRequest(...args) { return GM_xmlhttpRequest(...args)},
-        setClipboard(...args) { return GM_setClipboard(...args)},
-        getResourceText(...args) { return GM_getResourceText(...args)},
-        info() { return GM_info }
+        setValue (...args) { return GM_setValue(...args) },
+        getValue (...args) { return GM_getValue(...args) },
+        addStyle (...args) { return GM_addStyle(...args)},
+        xmlhttpRequest (...args) { return GM_xmlhttpRequest(...args)},
+        setClipboard (...args) { return GM_setClipboard(...args)},
+        getResourceText (...args) { return GM_getResourceText(...args)},
+        info () { return GM_info }
     }
-        let dot = {
-        setValue(...args) { return GM.setValue(...args) },
-        getValue(...args) { return GM.getValue(...args) },
-        addStyle(...args) { return addCustomCSS(...args)},
-        xmlhttpRequest(...args) { return GM.xmlHttpRequest(...args)},
-        setClipboard(...args) { return GM.setClipboard(...args)},
-        info() { return GM_info }
+    let dot = {
+        setValue (...args) { return GM.setValue(...args) },
+        getValue (...args) { return GM.getValue(...args) },
+        addStyle (...args) { return addCustomCSS(...args)},
+        xmlhttpRequest (...args) { return GM.xmlHttpRequest(...args)},
+        setClipboard (...args) { return GM.setClipboard(...args)},
+        info () { return GM_info }
     }
 
     if (gmPrefix === "GM_") {
