4d06dcfde586b8ae4dc1bd761d61acc0f4d18cd6
aclist <92275929+aclist@users.noreply.github.com>
2023-07-18 02:15:44 +0900

    fix: move bug report into dropdown

--- mods/report-bug.user.js
+++ mods/report-bug.user.js
@@ -13,7 +13,7 @@ function bugReportInit (toggle) {
             let originURL = bareURL + "%23" + postID;
             let footer = `%0A%0AReposted from kbin:%0A${originURL}`;
             let postBody = item.querySelector('.content').innerText;
-            let postFooter = item.querySelector('footer menu');
+            let postFooter = item.querySelector('footer menu .dropdown ul');
             let newListItem = document.createElement('li');
             let newHref = document.createElement('a');
             newListItem.className = "kes-report-bug";
