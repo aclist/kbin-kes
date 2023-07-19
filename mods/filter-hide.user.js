547e3231e71b4cb57fd1e63f906cab7b874adb4f
twelph <twelph@gmail.com>
2023-07-19 17:38:50 -0500

    Update filter-hide.user.js

--- mods/filter-hide.user.js
+++ mods/filter-hide.user.js
@@ -2,14 +2,14 @@
 // @name         kbin filter hider
 // @namespace    https://github.com/twelph/kbin-mobile-userscripts/
 // @version      0.1
-// @description  Hide filter by time button
+// @description  Hide filter by type button
 // @author       Twelph
 // @match        https://kbin.social/*
 // @license      MIT
 // ==/UserScript==
 
 function hideFilterButton(toggle) {
-  let btn = document.querySelector('button[aria-label="Filter by time"]');
+  let btn = document.querySelector('button[aria-label="Filter by type"]');
 
   if (btn) {
     btn.style.display = toggle ? 'none' : 'block';
