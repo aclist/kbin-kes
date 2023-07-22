ee4a22935919a82e7c76da12b6ec8474da84baed
aclist <92275929+aclist@users.noreply.github.com>
2023-07-23 07:43:33 +0900

    fix: more granular selector

--- mods/always-more.user.js
+++ mods/always-more.user.js
@@ -1,5 +1,5 @@
 function moreInit(toggle){
-    const more = document.querySelectorAll('.more')
+    const more = document.querySelectorAll('.entry__body > .more')
     if (toggle) {
         more.forEach((item) => {
             item.click();
