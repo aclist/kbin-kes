c4e0e04c2f3ea1f24eeb3edea5bead279ce0fe1b
aclist <92275929+aclist@users.noreply.github.com>
2023-07-24 09:14:56 +0900

    fix: count total mods via funcobj

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      2.1.0-beta.49
+// @version      2.1.0-beta.50
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
@@ -287,7 +287,7 @@ function constructMenu (json, layoutArr, isNew) {
     }
     function activeModCount () {
         const set = JSON.parse(localStorage["kes-settings"])
-        const totalMods = Object.keys(set).length - 3 // exclude lastTab, lastPage, and dock keys
+        const totalMods = Object.keys(funcObj).length
         let activeMods = 0
         let c
         let key
