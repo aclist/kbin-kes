bb722cabe32bd04773b1f91d4be8fc8e4f52e3c5
aclist <92275929+aclist@users.noreply.github.com>
2023-07-26 09:51:39 +0900

    fix: force CSS

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      2.1.0-beta.66
+// @version      2.1.0-beta.67
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
