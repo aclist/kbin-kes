482c9364959e83547e38f3e4905cd2a8c1a31af1
aclist <92275929+aclist@users.noreply.github.com>
2023-07-29 12:01:08 +0900

    fix: remove sheet before insertion

--- mods/unblur.user.js
+++ mods/unblur.user.js
@@ -1,12 +1,13 @@
 function unblurInit (toggle) {
 
     const unblurCSS = `
-    .thumb-subject {
-    filter: none !important;
+    .thumb-subject, .image-filler {
+        filter: none !important;
     }
     `;
 
     if (toggle) {
+        safeGM("removeStyle", 'unblurred');
         safeGM("addStyle", unblurCSS, 'unblurred');
     } else {
         safeGM("removeStyle", 'unblurred');
