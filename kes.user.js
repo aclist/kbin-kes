b9140c50ad443472d76dd671f586ca3b836efaa3
aclist <92275929+aclist@users.noreply.github.com>
2023-07-30 12:50:44 +0900

    fix: JSON

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      2.2.0-beta.55
+// @version      2.2.0-beta.56
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
