fcf5aac33acf0970edd31ee5045acd8125e7595b
ncase <null>
2023-07-30 12:10:11 +0900

    feat: use safeGM

--- mods/hover-indicator.user.js
+++ mods/hover-indicator.user.js
@@ -8,60 +8,53 @@
 // @license      MIT
 // ==/UserScript==
 
-let styleElement;
-
-function applyOutlines() {
-    // user customizable vars
-    const settings = getModSettings('hover');
-    const color = settings.color;
-    const thickness = settings.thickness;
-
-    // remove the existing style element if it exists
-    if (styleElement && styleElement.parentNode) {
-        styleElement.parentNode.removeChild(styleElement);
-    }
-
-    // apply outlines to elements
-    const sels = [
-        "a",
-        "h1",
-        "h2",
-        "h3",
-        "h4",
-        "h5",
-        "h6",
-        "img",
-        "button",
-        "label",
-        "markdown-toolbar",
-        "textarea",
-        "i",
-        "time",
-        "small",
-        "div.content",
-        "ul",
-        "li",
-        "span",
-        "figure",
-        "input",
-        "div.checkbox",
-        "div.ts-wrapper"
-
-    ]
-    
-    const selectors = sels.join(':hover, ');
-    const outline = `${selectors} { outline: ${thickness}px solid ${color};}`;
-    const border = `p:not(div.content p):hover { border: ${thickness}px solid ${color};}`;
-
-    styleElement = document.createElement('style');
-    styleElement.innerText = outline + border;
-    document.head.appendChild(styleElement);
-}
-
 function hoverIndicator(toggle) {
     if (toggle) {
         applyOutlines();
     } else {
-        document.head.removeChild(styleElement);
+        safeGM("removeStyle", "kes-hover-css")
+    }
+
+    function applyOutlines() {
+        const settings = getModSettings('hover');
+        const color = settings.color;
+        const thickness = settings.thickness;
+
+        const sels = [
+            "a",
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "img",
+            "button",
+            "label",
+            "markdown-toolbar",
+            "textarea",
+            "i",
+            "time",
+            "small",
+            "div.content",
+            "ul",
+            "li",
+            "span",
+            "figure",
+            "input",
+            "div.checkbox",
+            "div.ts-wrapper"
+
+        ]
+        const selectors = sels.join(':hover, ');
+        const mergedCSS = `${selectors} {
+            outline: ${thickness}px solid ${color};
+        }
+        p:not(div.content p):hover {
+            border: ${thickness}px solid ${color};
+        }
+        `;
+        safeGM("removeStyle", "kes-hover-css")
+        safeGM("addStyle", mergedCSS, "kes-hover-css")
     }
 }
