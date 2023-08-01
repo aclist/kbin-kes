6880e6e064e43d8727d3e18bb879acafb3e3490e
aclist <92275929+aclist@users.noreply.github.com>
2023-08-01 15:24:16 +0900

    fix: handle mutations

--- mods/thread-checkmarks.user.js
+++ mods/thread-checkmarks.user.js
@@ -1,34 +1,46 @@
-function checksInit (toggle) {
+function checksInit (toggle, mutation) {
     const settings = getModSettings('checks');
     const checkColor = settings["check-color"]
     const threadIndex = document.querySelector('[data-controller="subject-list"]')
     const user = document.querySelector('.login');
     const username = user.href.split('/')[4];
-    const test = document.querySelector('#kes-omni-check')
-    console.log(settings)
-    console.log(checkColor)
-    console.log(username)
+    const mut = mutation
+
     if ((!threadIndex) || (!username)) return
-    if (test) return
 
     async function fetchMags (username) {
         const loaded = await safeGM("getValue", 'omni-user-mags-' + username)
-        console.log(loaded)
+        if (!loaded) return
         setChecks(loaded)
     }
+    function addCheck(subs, item){
+        const mag = item.getAttribute('href').split('/')[2]
+        console.log("mag is:", mag)
+        if (subs.includes(mag)) {
+            addCheck(item);
+        }
+        const ch = document.createElement('span')
+        ch.style.color = getHex(checkColor);
+        ch.id = 'kes-omni-check'
+        ch.innerText = " ✓"
+        item.appendChild(ch)
+    }
     function setChecks (subs) {
-        console.log(subs)
-        document.querySelectorAll('.magazine-inline.instance').forEach((item) => {
-            const mag = item.getAttribute('href').split('/')[2]
-            if (subs.includes(mag)) {
-                const ch = document.createElement('span')
-                ch.style.color = getHex(checkColor);
-                ch.id = 'kes-omni-check'
-                ch.innerText = " ✓"
-                item.appendChild(ch)
+        if (mut) {
+            console.log("caught mutation")
+            if (mut.children[0].id === 'kes-omni-check') {
+                console.log("check already present")
+                return
+            } else {
+                console.log("adding check")
+                addCheck(subs, mut)
             }
-        });
-
+        } else {
+            console.log("no mutation, iterating")
+            document.querySelectorAll('.magazine-inline.instance').forEach((item) => {
+                addCheck(subs, item)
+            });
+        }
     }
 
     if (toggle) {
