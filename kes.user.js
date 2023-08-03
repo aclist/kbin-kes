08eb3cae9e7d6c820b532be07fc1b0d582fe08b6
aclist <92275929+aclist@users.noreply.github.com>
2023-08-03 14:36:16 +0900

    fix: drop placeholder

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      2.2.0-beta.134
+// @version      2.2.0-beta.136
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
