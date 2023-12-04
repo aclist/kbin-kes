8608b030ffa3fa8678c192ecf7f7625b7b977b88
Pamasich <pamasich@gmail.com>
2023-12-04 16:07:22 +0100

    chore: just some comment changes

--- mods/fix-codeblocks/fix-codeblocks.user.js
+++ mods/fix-codeblocks/fix-codeblocks.user.js
@@ -1,10 +1,11 @@
 function fixLemmyCodeblocks(toggle) {
+    // included everything inside here because of the private functions advisory
 
     const testPattern = /\n?<span style="color:#323232;">(.+\n)+<\/span>\n?/;
     const startTagPattern = /^\n?<span style="color:#323232;">/;
     const endTagPattern = /\n<\/span>\n?$/;
     const combinedPattern = /^<\/span><span style="color:#323232;">/gm;
-    // matches any line except the first one
+    // matches any line (or rather the start thereof) except the first one
     const startOfNewLinePattern = /(?<=\n)^/gm;
 
     const attribute = "data-code-fixed";
@@ -21,6 +22,7 @@ function fixLemmyCodeblocks(toggle) {
         }
     }
 
+    // including this to keep the mod's behavior consistent with the others
     function revertCodeblock(code) {
         const text = code.innerText.replaceAll(startOfNewLinePattern, `${endTag}${startTag}`);
         code.innerText = `\n${startTag}${text}\n${endTag}\n`;
