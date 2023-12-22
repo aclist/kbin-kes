3808b33d07344d337407085d7166ae170bb65273
aclist <92275929+aclist@users.noreply.github.com>
2023-12-22 16:18:36 +0900

    chore: update manifest

--- kes.user.js
+++ kes.user.js
@@ -28,52 +28,52 @@
 // @icon         https://kbin.social/favicon.svg
 // @connect      raw.githubusercontent.com
 // @connect      github.com
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/helpers/safegm.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/helpers/kbin-mod-options.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/helpers/safegm.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/helpers/kbin-mod-options.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/adjust/adjust.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/alpha-sort-subs/alpha-sort-subs.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/always-more/always-more.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/clarify-recipient/clarify-recipient.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/code-highlighting/code-highlighting.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/dropdown/dropdown.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/easy-emoticon/easy-emoticon.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/expand-posts/expand-posts.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/fix-codeblocks/fix-codeblocks.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hide-downvotes/hide-downvotes.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hide-logo/hide-logo.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hide-posts/hide-posts.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hide-reputation/hide-reputation.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hide-sidebar/hide-sidebar.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hide-thumbs/hide-thumbs.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hide-upvotes/hide-upvotes.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/hover-indicator/hover-indicator.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/improved-collapsible-comments/improved-collapsible-comments.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/kbin-federation-awareness/kbin-federation-awareness.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/label/label.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/mag-instance-names/mag-instance-names.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/mail/mail.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/mobile-cleanup/mobile-cleanup.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/move-federation-warning/move-federation-warning.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/nav-icons/nav-icons.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/notifications-panel/notifications-panel.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/omni/omni.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/rearrange/rearrange.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/report-bug/report-bug.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/resize-text/resize-text.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/softblock/softblock.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/subs/subs.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/thread-checkmarks/thread-checkmarks.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/thread-delta/thread-delta.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/timestamp/timestamp.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/unblur/unblur.user.js
-// @require      https://github.com/aclist/kbin-kes/raw/patch/thread-delta/mods/user-instance-names/user-instance-names.user.js
-// @resource     kes_layout https://github.com/aclist/kbin-kes/raw/patch/thread-delta/helpers/ui.json
-// @resource     kes_json https://github.com/aclist/kbin-kes/raw/patch/thread-delta/helpers/manifest.json
-// @resource     kes_css https://github.com/aclist/kbin-kes/raw/patch/thread-delta/helpers/kes.css
-// @downloadURL  https://github.com/aclist/kbin-kes/raw/patch/thread-delta/kes.user.js
-// @updateURL    https://github.com/aclist/kbin-kes/raw/patch/thread-delta/kes.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/adjust/adjust.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/alpha-sort-subs/alpha-sort-subs.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/always-more/always-more.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/clarify-recipient/clarify-recipient.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/code-highlighting/code-highlighting.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/dropdown/dropdown.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/easy-emoticon/easy-emoticon.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/expand-posts/expand-posts.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/fix-codeblocks/fix-codeblocks.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hide-downvotes/hide-downvotes.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hide-logo/hide-logo.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hide-posts/hide-posts.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hide-reputation/hide-reputation.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hide-sidebar/hide-sidebar.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hide-thumbs/hide-thumbs.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hide-upvotes/hide-upvotes.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/hover-indicator/hover-indicator.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/improved-collapsible-comments/improved-collapsible-comments.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/kbin-federation-awareness/kbin-federation-awareness.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/label/label.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/mag-instance-names/mag-instance-names.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/mail/mail.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/mobile-cleanup/mobile-cleanup.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/move-federation-warning/move-federation-warning.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/nav-icons/nav-icons.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/notifications-panel/notifications-panel.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/omni/omni.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/rearrange/rearrange.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/report-bug/report-bug.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/resize-text/resize-text.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/softblock/softblock.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/subs/subs.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/thread-checkmarks/thread-checkmarks.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/thread-delta/thread-delta.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/timestamp/timestamp.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/unblur/unblur.user.js
+// @require      https://github.com/aclist/kbin-kes/raw/testing/mods/user-instance-names/user-instance-names.user.js
+// @resource     kes_layout https://github.com/aclist/kbin-kes/raw/testing/helpers/ui.json
+// @resource     kes_json https://github.com/aclist/kbin-kes/raw/testing/helpers/manifest.json
+// @resource     kes_css https://github.com/aclist/kbin-kes/raw/testing/helpers/kes.css
+// @downloadURL  https://github.com/aclist/kbin-kes/raw/testing/kes.user.js
+// @updateURL    https://github.com/aclist/kbin-kes/raw/testing/kes.user.js
 // ==/UserScript==
 
 //START AUTO MASTHEAD
@@ -82,7 +82,7 @@
 const version = safeGM("info").script.version;
 const tool = safeGM("info").script.name;
 const repositoryURL = "https://github.com/aclist/kbin-kes/";
-const branch = "patch/thread-delta"
+const branch = "testing"
 const helpersPath = "helpers/"
 const branchPath = repositoryURL + "raw/" + branch + "/"
 const versionFile = branchPath + "VERSION";
