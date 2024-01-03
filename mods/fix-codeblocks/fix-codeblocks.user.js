448c4c3ab2b1c03c44f2dec79410447b9460bed1
Pamasich <pamasich@gmail.com>
2024-01-03 11:31:25 +0100

    refactor(fix-codeblocks): fixed code uses a separate element now
    I put the fixed code into its own code element and changed the previous
    code to be merely hidden. This allows the mod to better restore the
    original code especially now that the removed tags can have any color
    and the correct color needs to be restored.

--- mods/fix-codeblocks/fix-codeblocks.user.js
+++ mods/fix-codeblocks/fix-codeblocks.user.js
@@ -3,33 +3,36 @@ function fixLemmyCodeblocks (toggle) {
     const startTagPattern = /^\n?<span style="color:#[0-9a-fA-F]{6};">/;
     const endTagPattern = /\n<\/span>\n?$/;
     const combinedPattern = /^<\/span><span style="color:#[0-9a-fA-F]{6};">/gm;
-    // matches any line (or rather the start thereof) except the first one
-    const startOfNewLinePattern = /(?<=\n)^/gm;
 
-    const attribute = "data-code-fixed";
-    const startTag = '<span style="color:#323232;">';
-    const endTag = "</span>";
+    const fixedCodeAttribute = "data-fixed-code"
 
-    function fixCodeblock (code) {
-        if (testPattern.test(code.innerText)) {
-            code.innerText = code.innerText
-                .replace(startTagPattern, "")
-                .replaceAll(combinedPattern, "")
-                .replace(endTagPattern, "");
-            code.setAttribute(attribute, "");
-        }
+    /** @param {HTMLElement} codeblock */
+    function fixCodeblock (codeblock) {
+        if (!testPattern.test(codeblock.innerText)) return;
+
+        const fixedBlock = document.createElement("code");
+        fixedBlock.setAttribute(fixedCodeAttribute, "");
+        codeblock.parentNode.insertBefore(fixedBlock, codeblock.nextSibling);
+
+        fixedBlock.innerText = codeblock.innerText
+            .replace(startTagPattern, "")
+            .replace(combinedPattern, "")
+            .replace(endTagPattern, "");
+
+        codeblock.style.display = "none";
     }
 
-    // including this to keep the mod's behavior consistent with the others
-    function revertCodeblock (code) {
-        const text = code.innerText.replaceAll(startOfNewLinePattern, `${endTag}${startTag}`);
-        code.innerText = `\n${startTag}${text}\n${endTag}\n`;
-        code.removeAttribute(attribute);
+    /** @param {HTMLElement} fixedBlock */
+    function revertCodeblock (fixedBlock) {
+        /** @type {HTMLElement} */
+        const originalBlock = fixedBlock.previousElementSibling;
+        originalBlock.style.removeProperty("display");
+        fixedBlock.parentNode.removeChild(fixedBlock);
     }
 
     if (toggle) {
         document.querySelectorAll("pre code").forEach(fixCodeblock);
     } else {
-        document.querySelectorAll(`pre code[${attribute}]`).forEach(revertCodeblock);
+        document.querySelectorAll(`pre code[${fixedCodeAttribute}]`).forEach(revertCodeblock);
     }
 }
