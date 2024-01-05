5aa99b7b48a0e17df496079da6a679d26727695e
Pamasich <pamasich@gmail.com>
2024-01-05 13:54:44 +0100

    feat: Alternative access to All Content view

---
+++ mods/relocate-all-content/relocate-all-content.js
@@ -0,0 +1,20 @@
+/**
+ * This mod aims to make clicking the magazine name in the navbar lead to the All Content
+ * view instead of the Threads view, while removing the All Content button itself.
+*/
+function relocateAllContentView (toggle) {
+    const titleQuery = "div.head-title a";
+    const allContentQuery = "menu.head-nav__menu a[href^='/*/']";
+
+    const title = document.querySelector(titleQuery);
+    const currentUrl = title.getAttribute("href");
+    if (toggle && !currentUrl.startsWith("/*/")) title.setAttribute("href", `/*${currentUrl}`);
+    if (!toggle && currentUrl.startsWith("/*/")) title.setAttribute("href", currentUrl.slice(2));
+    
+    if (getModSettings("Alternative All Content Access")["hideAllContentButton"]) {
+        /** @type {HTMLElement} */
+        const allContentButton = [...document.querySelectorAll(allContentQuery)]
+            .find((element) => !element.isSameNode(title));
+        allContentButton.style.display = (toggle) ? "none" : "";
+    }
+}
