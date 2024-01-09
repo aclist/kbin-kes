915eedf80116ca51c345b4dae2e685b451115bd6
Pamasich <pamasich@gmail.com>
2024-01-09 10:41:36 +0100

    chore(fix-codeblocks): added some information to the code
    Added a description of what the mod is supposed to do, and what my future plans are with it. This commit also includes a Todo section, but that'll be removed before pushing. Information on testing will be filled in in a later commit.

--- mods/fix-codeblocks/fix-codeblocks.user.js
+++ mods/fix-codeblocks/fix-codeblocks.user.js
@@ -1,3 +1,29 @@
+/**
+ * Lemmy federates its code blocks with syntax highlighting, but /kbin doesn't currently 
+ * correctly handle that. It just displays the additional <span> tags for the syntax
+ * highlighting in plain text. This makes the code very hard to read.
+ * 
+ * This mod fixes the issue by removing those erroneous tags.
+ * 
+ * Testing:
+ * 
+ * Todo:
+ * - Explain the basic procedure to test that this works correctly
+ * - Set up (possibly automated) unit tests
+ * - Check if anything not yet covered needs to be. Does only Rust currently
+ * have syntax highlighting federated? The federated highlighting and the
+ * highlighting I can see on lemm.ee are very similar but not the same,
+ * indicating that they're different implementations yet use similar rules.
+ * Check if there's some common standardized style they use which could serve
+ * as a reference.
+ * 
+ * Future Plans:
+ * - Try to make sure this doesn't replace anything within strings.
+ * - Maybe make a version that makes the federated syntax highlighting functional rather 
+ * than removing it.
+ * 
+ * @param {boolean} isActive
+ */
 function fixLemmyCodeblocks (isActive) { // eslint-disable-line no-unused-vars
     const stylePattern = "((font-style:italic|font-weight:bold);)?color:#[0-9a-fA-F]{6};";
 
