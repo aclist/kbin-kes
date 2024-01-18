48699fc1d815e429e6c100814f94aa111cab3095
aclist <92275929+aclist@users.noreply.github.com>
2024-01-18 20:42:12 +0900

    chore: normalize version

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      3.2.0-beta.27
+// @version      3.2.2-beta.2
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
