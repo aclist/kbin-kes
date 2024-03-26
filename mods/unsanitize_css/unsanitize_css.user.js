c9d0de5e3d115308df99700a608e9a66ca8e86c4
Pamasich <pamasich@gmail.com>
2024-03-26 14:42:16 +0100

    feat: (new mod) fix broken css by reverting erroneously applied HTML sanitization

---
+++ mods/unsanitize_css/unsanitize_css.user.js
@@ -0,0 +1,56 @@
+/**
+ * Kbin currently wrongly sanitizes its custom CSS (as defined by magazines and users),
+ * causing some characters to be replaced by HTML escape codes. This breaks CSS rules involving,
+ * for example, the direct descendant selector (>) or nested CSS using the & character.
+ * 
+ * This mod aims to fix that issue until kbin does.
+ * 
+ * @todo Check whether this is *needed* on mbin, which will require creating a magazine with
+ * custom css there. It's not a priority though as long as the mod doesn't cause issues on mbin.
+ * 
+ * @param {Boolean} isActive Whether the mod has been turned on
+ */
+function fixWronglySanitizedCss (isActive) { // eslint-disable-line no-unused-vars
+    if (isActive) {
+        setup();
+    } else {
+        teardown();
+    }
+
+    function setup () {
+        var dummy = document.createElement("div");
+        document.querySelectorAll("style:not([id])").forEach((style) => {
+            dummy.innerHTML = style.textContent;
+            if (dummy.innerHTML != dummy.textContent) {
+                style.textContent = dummy.textContent;
+                markAsUnsanitized(style);
+            }
+        });
+        dummy.remove();
+    }
+
+    function teardown () {
+        var dummy = document.createElement("div");
+        Array.of(document.querySelectorAll("style:not([id])"))
+            .filter((style) => isUnsanitized(style))
+            .forEach((style) => {
+                dummy.textContent = style.textContent;
+                style.textContent = dummy.innerHTML;
+                markAsSanitized(style);
+            });
+        dummy.remove();
+    }
+
+    /** @param {HTMLElement} elem @returns {Boolean} */
+    function isUnsanitized (elem) {
+        return elem.dataset?.unsanitized;
+    } 
+    /** @param {HTMLElement} elem */
+    function markAsUnsanitized (elem) {
+        elem.dataset.unsanitized = true;
+    }
+    /** @param {HTMLElement} elem */
+    function markAsSanitized (elem) {
+        delete elem.dataset.unsanitized;
+    }
+}
