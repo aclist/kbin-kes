43eb7a74748a80f7f95fa1295f53f2ef302c9d65
aclist <92275929+aclist@users.noreply.github.com>
2024-03-27 14:10:13 +0900

    feat: omni depends

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      3.2.4-beta.40
+// @version      3.2.4-beta.41
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
