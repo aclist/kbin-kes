81abc38651f534d3deb81ed278b1c56914f3a486
aclist <92275929+aclist@users.noreply.github.com>
2024-03-31 09:26:05 +0900

    fix: css

--- helpers/funcs.js
+++ helpers/funcs.js
@@ -3569,15 +3569,17 @@ const funcObj = {
     function addPrefix (toggle) { // eslint-disable-line no-unused-vars 
 
         const settings = getModSettings("submission_label");
-        const label = settings["label"]
+        const label = settings["prefix"]
+        console.log(label)
         const css = `
-            .article:not(.entry-cross) > .entry__meta .user-inline::before {
-                content: " ${label} "
-                font-weight: 400;
-            }
+        article:not(.entry-cross) .user-inline::before {
+            content: " ${label} ";
+            font-weight: 400;
+        }
         `;
 
         if (toggle) {
+            safeGM("removeStyle", "submission-css")
             safeGM("addStyle", css, "submission-css")
         } else {
             safeGM("removeStyle", "submission-css")
