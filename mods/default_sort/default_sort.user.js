ff69562c51418c7f167e5f447a2dee247897a4a0
Pamasich <pamasich@gmail.com>
2024-04-12 11:28:24 +0200

    fix(default_sort): added general mbin compatibility
    `/` and `/home` still don't work

--- mods/default_sort/default_sort.user.js
+++ mods/default_sort/default_sort.user.js
@@ -39,7 +39,9 @@ function defaultSort (isActive) {  // eslint-disable-line no-unused-vars
         const defaultSort = determineDefaultSort();
         if (defaultSort == null) return;
 
-        const option = sortOptions.find((option) => option.textContent.trim() == defaultSort);
+        const option = sortOptions.find(
+            (option) => option.textContent.toLowerCase().trim() == defaultSort
+        );
         // if the default sort option doesn't exist in the option array, abort the mod
         if (option == undefined) return;
         // ensure the option links to the correct page
@@ -79,13 +81,13 @@ function defaultSort (isActive) {  // eslint-disable-line no-unused-vars
         // actually make the changes
         for (var option of options) {
             const hrefTokens = option.pathname.split('/');
-            if (hrefTokens[hrefTokens.length-1] != option.textContent.trim()) {
+            if (hrefTokens[hrefTokens.length-1] != option.textContent.toLowerCase().trim()) {
                 option.dataset.defaultSort_pathNameBeforeEdit = option.pathname;
                 if (isCommentsPage) {
-                    option.pathname += `/-/${option.textContent.trim()}`;
+                    option.pathname += `/-/${option.textContent.toLowerCase().trim()}`;
                 } else {
                     option.pathname += option.pathname.endsWith('/') ? '' : '/';
-                    option.pathname += option.textContent.trim();
+                    option.pathname += option.textContent.toLowerCase().trim();
                 }
             }
         }
@@ -107,11 +109,19 @@ function defaultSort (isActive) {  // eslint-disable-line no-unused-vars
      * @returns {HTMLAnchorElement[]}
      */
     function getSortOptions () {
-        var results =  Array.from(document.querySelectorAll(
+        // kbin
+        var results = Array.from(document.querySelectorAll(
             "aside.options:has(menu.options__filters) > menu.options__main a"
         ));
         if (results.length == 0) results = Array.from(document.querySelectorAll(
             "aside.options:has(menu.options__layout) > menu.options__main a"
+        ));
+        // mbin
+        if (results.length == 0) results = Array.from(document.querySelectorAll(
+            "li.dropdown:has(button[aria-label='Sort by']) a"
+        ));
+        if (results.length == 0) results = Array.from(document.querySelectorAll(
+            "aside.options:has(menu.options__view) > menu.options__main a"
         ));
         return results;
     }
@@ -135,7 +145,7 @@ function defaultSort (isActive) {  // eslint-disable-line no-unused-vars
     function isCurrentPageExplicitlySorted (sortOptions) {
         // The boosts page on the profile has its own way of sorting
         if (isBoostsPage() && window.location.search != '') return true;
-        const allowedRoutes = sortOptions.map((option) => option.textContent.trim());
+        const allowedRoutes = sortOptions.map((option) => option.textContent.toLowerCase().trim());
         const currentRoute = window.location.pathname;
         for (var route of allowedRoutes) {
             if (currentRoute.endsWith(`/${route}`)) return true;
@@ -165,6 +175,10 @@ function defaultSort (isActive) {  // eslint-disable-line no-unused-vars
         if (pathTokens.length == 1 && pathTokens[0] == "magazines") {
             // list of magazines
             return defaultSort(SupportedPages.MAGAZINE);
+        }
+        if (pathTokens.length == 1 && pathTokens[0] == "home") {
+            // mbin's `/` route
+            return defaultSort(SupportedPages.THREAD);
         }
         if (pathTokens.length == 1 && (pathTokens[0] == "sub" || pathTokens[0] == "all")) {
             return defaultSort(SupportedPages.THREAD);
