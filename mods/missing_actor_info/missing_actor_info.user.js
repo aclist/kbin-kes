abd9361e5bbd9fea99986f9583e3902fc3ac5300
Pamasich <pamasich@gmail.com>
2024-06-24 16:39:10 +0200

    feat(missing_actor_info): added retry ui
    My text advises users to check they wrote the name and domain correctly. This commit adds a UI to simplify that, providing both of them in different input fields with a button to (re)load the actor given in the input fields.

--- mods/missing_actor_info/missing_actor_info.user.js
+++ mods/missing_actor_info/missing_actor_info.user.js
@@ -21,8 +21,8 @@ function missingActorInfo (isActive) { // eslint-disable-line no-unused-vars
         It seems you've tried to access a user from this instance, but such a user does not exist.
         <br/>Please verify you've written their name correctly.
         <br/><br/>
-        If you meant to access a remote user, you should append their instance after the username.
-        <br/><br/>
+        If you meant to access a remote user, you should append their instance's domain after 
+        the username.<br/><br/>
         <add form to edit in a domain, and a retry button>
     `;
     const EXPLAIN_LOCAL_MAG = `
@@ -30,7 +30,7 @@ function missingActorInfo (isActive) { // eslint-disable-line no-unused-vars
         exist.<br/>
         Please verify you've written its name correctly.
         <br/><br/>
-        If you meant to access a remote magazine, you should append its instance after the 
+        If you meant to access a remote magazine, you should append its instance's domain after the 
         magazine's name.
         <br/><br/>
         <add form to edit in a domain, and a retry button><br/>
@@ -46,7 +46,7 @@ function missingActorInfo (isActive) { // eslint-disable-line no-unused-vars
         <br/><br/>
         To prompt ${INSTANCE_NAME} to create a {page} page for this URL, search for it on the 
         search page. This URL will then be available within moments.<br/>
-        To actually see new posts federated over however, you'll need to subscribe to the {page}.
+        To actually see new posts federated over however, you will need to subscribe to the {page}.
         <br/><br/>
         <add shortcut to search the magazine or user><br/>
         <add link to the remote page>
@@ -79,6 +79,54 @@ function missingActorInfo (isActive) { // eslint-disable-line no-unused-vars
         else text = EXPLAIN_LOCAL_MAG;
         text = text.replace(replacePage, pageType);
         parent.append(buildParagraph(text));
+        addControls(parent, pageType);
+    }
+
+    /**
+     * @param {HTMLElement} parent
+     * @param {'user' | 'magazine'} pageType
+     */
+    function addControls (parent, pageType) {
+        parent.append(buildEditURLUI());
+    }
+
+    /** @returns {HTMLElement} @param {'user' | 'magazine'} pageType  */
+    function buildEditURLUI (pageType) {
+        const path = separateResourcePath(window.location.pathname);
+        /** @type {HTMLInputElement} */
+        const nameField = create('input');
+        /** @type {HTMLInputElement} */
+        const domainField = create('input');
+        const reloadBtn = create('a');
+        reloadBtn.textContent = "Reload";
+        nameField.value = path.resource;
+        domainField.value = path.domain ?? "";
+        domainField.placeholder = "(when local, leave blank)";
+        reloadBtn.style.marginLeft = '.5rem';
+        reloadBtn.className = 'btn btn-link btn__primary';
+        reloadBtn.style.paddingTop = '0.2rem';
+        reloadBtn.style.paddingBottom = '0.3rem';
+
+        function getNewLink () {
+            var value = (pageType == 'user' ? '/u/' : '/m/') + nameField.value;
+            if (domainField.value != '') value += '@' + domainField.value;
+            return value;
+        }
+        reloadBtn.href = getNewLink();
+        nameField.addEventListener('input', () => reloadBtn.href = getNewLink());
+        domainField.addEventListener('input', () => reloadBtn.href = getNewLink());
+
+        const div = create('div');
+        div.style.color = 'inherit';
+        div.style.fontSize = 'var(--kbin-body-font-size)';
+        div.style.fontWeight = 'var(--kbin-body-font-weight)';
+        div.append("Edit Name & Domain:", create("br"), nameField, "@", domainField, reloadBtn);
+        return div;
+    }
+
+    /** @returns {HTMLElement} @param {string} e */
+    function create (e) {
+        return document.createElement(e);
     }
 
     /**
@@ -97,7 +145,7 @@ function missingActorInfo (isActive) { // eslint-disable-line no-unused-vars
      * @returns {HTMLElement}
      */
     function buildParagraph (text) {
-        const p = document.createElement('p');
+        const p = create('p');
         p.innerHTML = text;
         p.style.color = 'inherit';
         p.style.fontSize = 'var(--kbin-body-font-size)';
@@ -110,7 +158,7 @@ function missingActorInfo (isActive) { // eslint-disable-line no-unused-vars
      * @returns {HTMLElement}
      */
     function buildTitle (text) {
-        const title = document.createElement('h2');
+        const title = create('h2');
         title.innerHTML = text;
         title.style.color = 'var(--kbin-text-muted-color)';
         title.style.marginBlockStart = 0;
