67f32c0635e9a1fbe0edafdcc5b8758ae3569298
aclist <92275929+aclist@users.noreply.github.com>
2024-08-03 13:21:09 +0900

    feat: add settings to collapse pins (WIP)

--- kes.user.js
+++ kes.user.js
@@ -2,11 +2,10 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      4.2.0-beta.19
+// @version      4.2.0-beta.20
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
-// @match        https://kbin.run/*
 // @match        https://lab2.kbin.pub/*
 // @match        https://lab3.kbin.pub/*
 // @match        https://fedia.io/*
