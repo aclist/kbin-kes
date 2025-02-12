c50fa2951112896a100a287e55fc734dd4bdbe24
aclist <92275929+aclist@users.noreply.github.com>
2025-02-12 19:30:33 +0900

    chore: update alpha sort subs manifest

--- helpers/funcs.js
+++ helpers/funcs.js
@@ -3199,44 +3199,56 @@ const funcObj = { // eslint-disable-line no-unused-vars
 
     alpha_sort_subs: //mes-func
     function alphaSortInit (toggle) { // eslint-disable-line no-unused-vars
-        const ind = window.location.href.split('/')[5]
-        if (!ind) return
-        if ((ind.indexOf('subscriptions') < 0) && (ind.indexOf('followers') < 0)) return
-        const ul = document.querySelector('.section.magazines.magazines-columns ul,.section.users.users-columns ul')
-        const obj = {}
 
-        if (toggle) {
-            const mags = document.querySelectorAll('.section.magazines.magazines-columns ul li a,.section.users.users-columns ul li a');
-            const namesArr = []
-
-            mags.forEach((item) => {
-                const dest = item.href;
-                const hrName = item.innerText;
-                obj[hrName] = dest
-                namesArr.push(hrName);
-            });
-
-            const sorted = namesArr.sort((a, b) => {
-                return a.localeCompare(b, undefined, { sensitivity: 'base' });
-            });
-
-            const outer = document.querySelector('.section.magazines.magazines-columns,.section.users.users-columns')
-            $(ul).hide();
-
-            for (let i =0; i<sorted.length; ++i) {
-                const myListItem = document.createElement('li');
-                myListItem.className = "alpha-sorted-subs"
-                const mySubsLink = document.createElement('a');
-                mySubsLink.setAttribute('href', obj[sorted[i]]);
-                mySubsLink.innerText = namesArr[i];
-                mySubsLink.className = 'subs-nav';
-                myListItem.append(mySubsLink);
-                outer.append(myListItem);
+        function compare (a, b) {
+            function _getMagName (magEl) {
+                return magEl.querySelector('.stretched-link').innerText.toUpperCase();
             }
 
+            return _getMagName(a) > _getMagName(b) ? 1: -1
+        }
+
+        const pt = getPageType(); // eslint-disable-line no-undef
+        let list_columns
+        switch (pt) {
+            case "Mbin.User.Subscriptions": {
+                list_columns = '.magazines-columns'
+                break;
+            }
+            case "Mbin.User.Followers":
+            case "Mbin.User.Following": {
+                list_columns = '.users-columns'
+                break;
+            }
+            default:
+                return
+        }
+        const columns = document.querySelector(list_columns)
+
+        if (toggle) {
+            const ul = columns.querySelector('ul');
+            const mags = ul.querySelectorAll('li');
+            const arr = [];
+
+            mags.forEach((mag) => {
+                arr.push(mag);
+            })
+
+            const ulCloned = document.createElement('ul');
+            ulCloned.id = "mes-alpha-sort";
+            arr.sort(compare);
+            arr.forEach((mag)=>{
+                //perform deep copy of children
+                const clone = mag.cloneNode(true);
+                ulCloned.appendChild(clone);
+            })
+            ul.style.display = "none";
+            ul.after(ulCloned);
         } else {
-            $('.alpha-sorted-subs').remove();
-            $(ul).show();
+            const ul = columns.querySelector('ul');
+            ul.style.display = "";
+            const ulCloned = document.querySelector('#mes-alpha-sort');
+            ulCloned.remove();
         }
     },
 
