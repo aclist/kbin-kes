8a2d6cf6fb628c73c958b96952204843dffffeeb
Pamasich <pamasich@gmail.com>
2025-02-13 14:13:37 +0100

    feat(hide_sidebar): added option to expand the main content if the sidebar is hidden

--- mods/hide_sidebar/hide_sidebar.user.js
+++ mods/hide_sidebar/hide_sidebar.user.js
@@ -23,10 +23,23 @@ function hideSidebar (toggle) { // eslint-disable-line no-unused-vars
                 $(obj[key]).show();
             }
         }
+        // expand the content to cover the space freed up by hiding the sidebar
+        const main = document.querySelector('.mbin-container > #main');
+        if (settings["sidebar"] && settings["expand"]) {
+            main.style.gridColumn = "span 2";
+        } else {
+            if (main.style.gridColumn == "span 2") {
+                main.style.gridColumn = '';
+            }
+        }
     } else {
         for (let i = 0; i< keys.length; i++) {
             let key = keys[i]
             $(obj[key]).show();
+        }
+        const main = document.querySelector('.mbin-container > #main');
+        if (main.style.gridColumn == "span 2") {
+            main.style.gridColumn = '';
         }
     }
 }
