5f55967ba40647a79ad61ef8ae53d393ed1956eb
aclist <92275929+aclist@users.noreply.github.com>
2025-02-17 20:12:09 +0900

    fix: add missing pages.js file

---
+++ helpers/pages.js
@@ -0,0 +1,37 @@
+const Mbin = Object.freeze({ //eslint-disable-line no-unused-vars
+    Top: 1,
+    Search: 2,
+    Magazines: 3,
+    People: 4,
+    Bookmarks: 5,
+    Tag: 6,
+    Microblog: 7,
+    Settings: 8,
+    Magazine: 9,
+    Messages: Object.freeze({
+        Inbox: 1,
+        Notifications: 2,
+        Thread: 3
+    }),
+    User: Object.freeze({
+        Default: 1,
+        DirectMessage: 2,
+        Subscriptions: 3,
+        Threads: 4,
+        Comments: 5,
+        Posts: 6,
+        Replies: 7,
+        Boosts: 8,
+        Following: 9,
+        Followers: 10
+    }),
+    Thread: Object.freeze({
+        Comments: 1,
+        Favorites: 2,
+        Boosts: 3
+    }),
+    Domain: Object.freeze({
+        Default: 1,
+        Comments: 2
+    })
+})
