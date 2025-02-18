b386760f72598d5f9e9eb60ed47a51ae1145a6dd
aclist <92275929+aclist@users.noreply.github.com>
2025-02-19 05:27:45 +0900

    chore: bump version

--- helpers/funcs.js
+++ helpers/funcs.js
@@ -21,6 +21,28 @@ const funcObj = { // eslint-disable-line no-unused-vars
         }
     },
 
+    suppress_cover: //mes-func
+    function suppressCoverInit (toggle) { //eslint-disable-line no-unused-vars
+        const pt = getPageType();
+        switch (pt) {
+            case Mbin.Thread.Comments:
+            case Mbin.Thread.Favorites:
+            case Mbin.Thread.Boosts:
+            case Mbin.Magazine:
+                break;
+            default:
+                return
+        }
+
+        const cover = document.querySelector('#sidebar .magazine.section figure');
+
+        if (toggle) {
+            cover.style.display = "none"
+        } else {
+            cover.style.display = "block"
+        }
+    },
+
     improved_collapsible_comments: //mes-func
     function initCollapsibleComments (toggle, mutation) { // eslint-disable-line no-unused-vars
         function applyCommentStyles () {
