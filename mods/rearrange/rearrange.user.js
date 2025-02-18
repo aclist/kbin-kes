73b8b08ebdb77354c2c82b81c76d258c519390c0
aclist <92275929+aclist@users.noreply.github.com>
2025-02-19 01:57:44 +0900

    fix: apply on thread pages

--- mods/rearrange/rearrange.user.js
+++ mods/rearrange/rearrange.user.js
@@ -1,18 +1,21 @@
 function rearrangeInit (toggle) { // eslint-disable-line no-unused-vars
     function rearrangeSetup () {
-        if (window.location.href.split('#')[1] != 'comments') return
+        const pt = getPageType();
+        if (pt !== Mbin.Thread.Comments) return
         const settings = getModSettings('rearrange');
         const content = document.querySelector('#content');
         content.style.display = 'grid';
         const op = document.querySelector('.section--top');
         const activity = document.querySelector('#activity');
-        const post = document.querySelector('#comment-add');
         const options = document.querySelector('#options');
         const comments = document.querySelector('#comments');
 
         op.style.order = settings["op"]
         activity.style.order = settings["activity"]
-        post.style.order = settings["post"]
+        if (isLoggedIn()) {
+            const post = document.querySelector('#comment-add');
+            post.style.order = settings["post"]
+        }
         options.style.order = settings["options"]
         comments.style.order = settings["comments"]
     }
