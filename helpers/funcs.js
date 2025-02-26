b7d91abaaff33ad3a6bf19806a90a32022af57d5
aclist <92275929+aclist@users.noreply.github.com>
2025-02-26 09:02:41 +0900

    chore: bump version

--- helpers/funcs.js
+++ helpers/funcs.js
@@ -1851,6 +1851,60 @@ const funcObj = { // eslint-disable-line no-unused-vars
         }
     },
 
+    hide_related: //mes-func
+    function toggleLogo (toggle) { // eslint-disable-line no-unused-vars
+
+        function isIndex () {
+            const pt = getPageType();
+            switch (pt) {
+                case Mbin.Domain.Default:
+                case Mbin.Domain.Comments:
+                case Mbin.Top:
+                    return true
+                default:
+                    return false
+            }
+        }
+        function isThread () {
+            const pt = getPageType();
+            switch (pt) {
+                case Mbin.Thread.Comments:
+                case Mbin.Thread.Favorites:
+                case Mbin.Thread.Boosts:
+                    return true
+                default:
+                    return false
+            }
+        }
+
+        function hideRelated () {
+            restoreRelated();
+            const settings = getModSettings("hide_related");
+            if ((settings["index"]) && (isIndex())) {
+                document.querySelectorAll(".entry-cross").forEach((entry) => {
+                    entry.style.display = "none"
+                })
+            }
+            if ((settings["thread"]) && (isThread())) {
+                document.querySelectorAll(".entries-cross").forEach((entry) => {
+                    entry.style.display = "none"
+                });
+            }
+        }
+
+        function restoreRelated () {
+            document.querySelectorAll(".entry-cross").forEach((entry) => {
+                entry.style.removeProperty("display");
+            })
+            document.querySelectorAll(".entries-cross").forEach((entry) => {
+                entry.style.removeProperty("display");
+            })
+        }
+
+        if (toggle) hideRelated();
+        if (!toggle) restoreRelated();
+    },
+
     remove_ads: //mes-func
     function filter (toggle, mutation) { // eslint-disable-line no-unused-vars
 
