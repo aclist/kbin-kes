5843bb9afc019c994a639b8a52d3f2b5d6d791c6
aclist <92275929+aclist@users.noreply.github.com>
2025-02-26 09:10:04 +0900

    chore: bump version

--- helpers/funcs.js
+++ helpers/funcs.js
@@ -2629,6 +2629,16 @@ const funcObj = { // eslint-disable-line no-unused-vars
         const kibby = `${prefix}/kbin_logo_kibby.svg`
         const kibbyMini = `${prefix}/kibby-mini.svg`
         const kbinMini = `${prefix}/kbin-mini.svg`
+        const mbinNoText = "https://raw.githubusercontent.com/MbinOrg/mbin/refs/heads/main/assets/images/sources/mbin-notext.svg"
+        const mbin = "https://raw.githubusercontent.com/MbinOrg/mbin/refs/heads/main/assets/images/sources/mbin-logo.svg"
+
+        const logos = {
+            "Kbin (no text)": kbinMini,
+            "Kibby": kibby,
+            "Kibby (no text)": kibbyMini,
+            "Mbin (no text)": mbinNoText,
+            "Mbin": mbin
+        }
 
         function getDefaultLogo () {
             const keyw = document.querySelector('meta[name="keywords"]').content.split(',')[0]
@@ -2638,36 +2648,48 @@ const funcObj = { // eslint-disable-line no-unused-vars
 
         function updateLogo (link) {
             $('.brand a').show();
+            document.querySelector(".head-nav__menu").style.removeProperty("padding-left")
             const img = document.querySelector('.brand a img');
-            img.setAttribute("src", link);
+            //special handling for instances with text-only logo
+            if (!img) {
+                const sp = document.querySelector(".brand a span")
+                sp.style.display = "none"
+                const i = document.createElement("img");
+                i.setAttribute("src", link)
+                i.id = "logo"
+                sp.insertAdjacentElement("afterend", i)
+            } else {
+                img.setAttribute("src", link);
+            }
         }
 
         function changeLogo () {
             const ns = "changelogo";
-
             const settings = getModSettings(ns);
             let opt = settings["logotype"];
             switch (opt) {
                 case "Hidden":
                     updateLogo(getDefaultLogo())
                     $('.brand a').hide();
+                    document.querySelector(".head-nav__menu").style.paddingLeft = "20px"
                     break;
-                case "Kibby":
-                    updateLogo(kibby);
-                    break;
-                case "Kbin (no text)":
-                    updateLogo(kbinMini);
-                    break;
-                case "Kibby (no text)":
-                    updateLogo(kibbyMini);
-                    break;
+                default:
+                    updateLogo(logos[opt])
             }
         }
 
         function restoreLogo () {
             $('.brand').show();
+            //special handling for instances with text-only logo
+            const sp = document.querySelector(".brand a span");
+            const a = document.querySelector(".brand a");
+            if (sp) {
+                sp.style.removeProperty("display");
+                a.style.removeProperty("display");
+                document.querySelector(".brand a img").remove();
+                return
+            }
             updateLogo(getDefaultLogo());
-
         }
 
         if (toggle) {
