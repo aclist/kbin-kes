6743c5e5ba06edccee59f83a39176f93bfbacab0
aclist <92275929+aclist@users.noreply.github.com>
2025-03-03 15:07:12 +0900

    feat: adds a cosmetic separator

---
+++ mods/thread_separator/thread_separator.js
@@ -0,0 +1,15 @@
+function dividerInit (toggle) { //eslint-disable-line no-unused-vars
+    function insertSeparator () {
+        const pt = getPageType()
+        if (!isThread() && pt !== Mbin.Microblog) return
+        if (document.querySelector("#mes-thread-divider")) return
+        const top = document.querySelector(".section--top")
+        const sep = document.createElement("div")
+        top.insertAdjacentElement("beforebegin", sep)
+        sep.style.height = "11px"
+        sep.id = "mes-thread-divider"
+    }
+
+    if (toggle) insertSeparator()
+    if (!toggle) document.querySelector("#mes-thread-divider")?.remove();
+}
