29f27511328b0f66f2bc91a5c8ef4afb8079f3a2
aclist <92275929+aclist@users.noreply.github.com>
2025-03-07 12:31:07 +0900

    chore: bump version

--- kes.user.js
+++ kes.user.js
@@ -2,7 +2,7 @@
 // @name         KES
 // @namespace    https://github.com/aclist
 // @license      MIT
-// @version      4.3.0-beta.54
+// @version      4.3.0-beta.55
 // @description  Kbin Enhancement Suite
 // @author       aclist
 // @match        https://kbin.social/*
@@ -31,7 +31,7 @@
 // @connect      github.com
 // @require      https://raw.githubusercontent.com/aclist/kbin-kes/testing/helpers/safegm.user.js
 // @require      https://raw.githubusercontent.com/aclist/kbin-kes/testing/helpers/funcs.js
-// @require      https://raw.githubusercontent.com/aclist/kbin-kes/testing/helpers/pages.js
+// @require      https://raw.githubusercontent.com/aclist/kbin-kes/testing/helpers/enums.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @resource     kes_layout https://raw.githubusercontent.com/aclist/kbin-kes/testing/helpers/ui.json
