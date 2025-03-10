3b2c37fbae3925efec1ba232d9922add3369b4d4
aclist <92275929+aclist@users.noreply.github.com>
2025-03-10 21:24:38 +0900

    feat: add theme enums

--- helpers/safegm.user.js
+++ helpers/safegm.user.js
@@ -258,10 +258,23 @@ function getTheme () {
     let theme = undefined
     document.querySelector("body").classList.forEach((c) => {
         if (c.includes("theme--")) {
-            theme = c
+            theme = c.split("--")[1]
         }
     })
-    return theme
+    switch (theme) {
+        case "kbin":
+            return Theme.KBIN
+        case "dark":
+            return Theme.DARK
+        case "light":
+            return Theme.LIGHT
+        case "solarized-light":
+            return Theme.SOLARIZED_LIGHT
+        case "solarized-dark":
+            return Theme.SOLARIZED_DARK
+        case "tokyo-night":
+            return Theme.TOKYO_NIGHT
+    }
 }
 
 
