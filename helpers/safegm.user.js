91edbd8753216ec92ec8174ee350d3b36ac6b635
Pamasich <pamasich@gmail.com>
2025-03-13 09:55:35 +0100

    refactor(loadMags)!: the callback now decides when the code should run
    Removed the runCallbackOnlyOnce parameter in favor of telling the callback whether it's the final call. This way the same call can be used to run code that should run on every callback call and code which should only run on the last one.

--- helpers/safegm.user.js
+++ helpers/safegm.user.js
@@ -230,12 +230,11 @@ function getPageType () { //eslint-disable-line no-unused-vars
 
 /**
  * Loads the current user's subscriptions.
- * @param {function(string[]):void} callback
+ * @param {function(string[],boolean):void} callback
  * @param {string} ns
  * @param {boolean} useCache
- * @param {boolean} runCallbackOnlyOnce
  */
-async function loadMags (callback, ns, useCache=false, runCallbackOnlyOnce=false) {
+async function loadMags (callback, ns, useCache=false) {
     // make sure the user is logged in
     const username = document.querySelector('.login .user-name')?.textContent;
     if (!username) return;
@@ -246,16 +245,16 @@ async function loadMags (callback, ns, useCache=false, runCallbackOnlyOnce=false
     const cancelKey = `loadMags-${hostname}-${username}-${ns}`;
     safeGM("setValue", cancelKey, false);
 
-    async function runCallback (mags) {
+    async function runCallback (mags, isFinalCall) {
         if (safeGM("getValue", cancelKey)) return;
         safeGM("setValue",`user-mags-${hostname}-${username}`, mags);
-        callback(mags);
+        callback(mags, isFinalCall);
     }
 
     if (useCache) {
         const cachedValue = safeGM("getValue",`user-mags-${hostname}-${username}`);
         if (cachedValue && cachedValue.length > 0) {
-            runCallback(cachedValue);
+            runCallback(cachedValue, true);
             return;
         }
     }
@@ -276,25 +275,25 @@ async function loadMags (callback, ns, useCache=false, runCallbackOnlyOnce=false
                 loadFromPage(username, nextPage.getAttribute('href').split('=')[1], mags);
             } else {
                 // finished loading all pages
-                runCallback(mags);
+                runCallback(mags, true);
             }
         });
     }
     async function loadFromSidebar () {
         const magList = [...document.querySelectorAll('.subscription')];
         if (magList.length == 0) {
-            runCallback([]);
+            runCallback([], true);
             return;
         }
         const containsShowMore = magList[magList.length-1].querySelector('button') != undefined;
         loadedMags = (containsShowMore ? magList.slice(0,-1) : magList)
             .map((mag) => mag.querySelector('a').getAttribute('href').split('/')[2]);
-        if (!runCallbackOnlyOnce || !containsShowMore) {
-            runCallback(loadedMags);
-        } else if (containsShowMore) {
+        runCallback(loadedMags, !containsShowMore);
+        if (containsShowMore) {
             loadFromPage(username, 1);
         }
     }
+
     if (document.querySelector('.subscription-list') != undefined) {
         loadFromSidebar();
     } else {
