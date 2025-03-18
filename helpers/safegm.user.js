cbc21f651b974371a0ee508cd98eb46e88376666
aclist <92275929+aclist@users.noreply.github.com>
2025-03-18 15:33:51 +0900

    fix: magazine thread filters returning wrong enum

--- helpers/safegm.user.js
+++ helpers/safegm.user.js
@@ -225,6 +225,7 @@ function getPageType () { //eslint-disable-line no-unused-vars
             return Mbin.Domain.Default
         case "m":
             if (url[5] === undefined) return Mbin.Magazine
+            if (window.location.href.includes("/threads")) return Mbin.Magazine
             if (url[5] === "microblog") return Mbin.Microblog
             if ((url[5] === "t") && (window.location.href.includes("/favourites"))) return Mbin.Thread.Favorites
             if ((url[5] === "t") && (window.location.href.includes("/up"))) return Mbin.Thread.Boosts
