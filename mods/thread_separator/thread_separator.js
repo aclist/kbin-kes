34ee73768e3515275d7dd9d984c04bf9f61417c3
aclist <92275929+aclist@users.noreply.github.com>
2025-03-19 18:49:47 +0900

    fix: normalize height

--- mods/thread_separator/thread_separator.js
+++ mods/thread_separator/thread_separator.js
@@ -6,7 +6,7 @@ function dividerInit (toggle) { //eslint-disable-line no-unused-vars
         const top = document.querySelector(".section--top")
         const sep = document.createElement("div")
         top.insertAdjacentElement("beforebegin", sep)
-        sep.style.height = "11px"
+        sep.style.height = "0.5rem"
         sep.id = "mes-thread-divider"
     }
 
